b9250d7da0f99e4f843e40091555b7e09168ed7a
gellen <hellogellen@gmail.com>
2020-12-04 16:40:35 +0700

    Update layanan/index

--- application/views/layanan/index.php
+++ application/views/layanan/index.php
@@ -1,36 +1,56 @@
-<div class="row">
-    <div class="col-md-12">
-        <div class="box">
-            <div class="box-header">
-                <h3 class="box-title">Layanan Listing</h3>
-            	<div class="box-tools">
-                    <a href="<?php echo site_url('layanan/add'); ?>" class="btn btn-success btn-sm">Add</a> 
-                </div>
-            </div>
-            <div class="box-body">
-                <table class="table table-striped">
-                    <tr>
-						<th>Jenis</th>
-						<th>Nama</th>
-						<th>Durasi</th>
-						<th>Harga</th>
-						<th>Actions</th>
-                    </tr>
-                    <?php foreach($layanan as $l){ ?>
-                    <tr>
-						<td><?php echo $l['jenis']; ?></td>
-						<td><?php echo $l['nama']; ?></td>
-						<td><?php echo $l['durasi']; ?></td>
-						<td><?php echo $l['harga']; ?></td>
-						<td>
-                            <a href="<?php echo site_url('layanan/edit/'.$l['id_layanan']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Edit</a> 
-                            <a href="<?php echo site_url('layanan/remove/'.$l['id_layanan']); ?>" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span> Delete</a>
-                        </td>
-                    </tr>
-                    <?php } ?>
-                </table>
-                                
-            </div>
+<div class="columns">
+    <div class="column is-10">
+        <h1 class="title has-text-success font-family-philosopher">Daftar Layanan</h1>
+    </div>
+    <div class="column">
+        <div class="field">
+            <a href="<?= site_url('layanan/add'); ?>" class="button is-success">Tambah Layanan</a>
         </div>
     </div>
+</div>
+<div class="table-container">
+    <table class="table is-fullwidth">
+        <thead>
+            <tr>
+                <th>Jenis</th>
+                <th>Nama</th>
+                <th>Durasi</th>
+                <th>Harga</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <th>Jenis</th>
+                <th>Nama</th>
+                <th>Durasi</th>
+                <th>Harga</th>
+                <th>Actions</th>
+            </tr>
+        </tfoot>
+        <tbody>
+            <?php foreach ($layanan as $l) { ?>
+                <tr>
+                    <td><?php echo $l['jenis']; ?></td>
+                    <td><?php echo $l['nama']; ?></td>
+                    <td><?php echo $l['durasi']; ?></td>
+                    <td><?php echo $l['harga']; ?></td>
+                    <td>
+                        <a href="<?php echo site_url('layanan/edit/' . $l['id_layanan']); ?>" class="button is-small is-info">
+                            <span class="icon">
+                                <i class="fas fa-edit"></i>
+                            </span>
+                            <span>Edit</span>
+                        </a>
+                        <a href="<?php echo site_url('layanan/remove/' . $l['id_layanan']); ?>" class="button is-small is-danger">
+                            <span class="icon">
+                                <i class="fa fa-trash"></i>
+                            </span>
+                            <span> Delete </span>
+                        </a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 </div>
