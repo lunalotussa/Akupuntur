6651a2a39eb448b114b0060432aced3dc27d9e94
lunalotussa <171111047@mhs.stiki.ac.id>
2020-12-06 11:05:41 +0700

    Add files via upload
    update controller search -> function jasa & search

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -3,6 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Landing extends CI_Controller
 {
+     function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Search_model');
+    } 
 
     public function index()
     {
@@ -13,10 +18,21 @@ class Landing extends CI_Controller
 
     public function jasa()
     {
-        $this->load->view('templates/relish/header');
-        $this->load->view('landing/jasa');
-        $this->load->view('templates/relish/footer');
+        $data['landing']=$this->Search_model->get_all();
+        $data['_view'] = 'landing/jasa';
+        $this->load->view('templates/relish/header',$data);
+        $this->load->view('landing/jasa',$data);
+        $this->load->view('templates/relish/footer',$data);
     }
+
+    public function search(){
+        $keyword = $this->input->post('keyword');
+        $data['landing']=$this->Search_model->get_search_keyword($keyword);
+        $data['_view'] = 'landing/search';
+        $this->load->view('templates/relish/header',$data);
+        $this->load->view('landing/search',$data);
+        $this->load->view('templates/relish/footer',$data);
+        }
 
 }
 
