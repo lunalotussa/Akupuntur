7c977f69c746e25171738d0018f992ea077e25c1
Panji Iman Baskoro <panjidia995@gmail.com>
2020-12-10 20:25:39 +0700

    typo

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -72,7 +72,7 @@ class Cart extends CI_Controller
             $tot = 0;
             foreach($cart as $item){
                 $harga = $this->Cart_model->get_harga_layanan($item);
-                $tot += $harga[0];
+                $tot += $harga[0]->harga;
             }
             
             $params = array(
