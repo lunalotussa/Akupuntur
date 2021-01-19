258c5de58dd6db053bed43e847f7033a09381cb9
Panji Iman Baskoro <panjidia995@gmail.com>
2021-01-19 15:30:19 +0700

    fix query

--- application/views/cart/history.php
+++ application/views/cart/history.php
@@ -22,7 +22,7 @@
         </tfoot>
         <tbody>
             <?php 
-                $sql = "SELECT *,detail_transaksi.tanggal as tunggul FROM layanan a join detail_layanan b on a.id_layanan = b.id_layanan join cart c on b.id_detailLayanan = c.id_detail_layanan join detail_transaksi d on c.id_chart=d.id_cart join transaksi e on d.no_transaksi=e.no_transaksi where e.id_customer='$id_user'";
+                $sql = "SELECT *,d.tanggal as tunggul FROM layanan a join detail_layanan b on a.id_layanan = b.id_layanan join cart c on b.id_detailLayanan = c.id_detail_layanan join detail_transaksi d on c.id_chart=d.id_cart join transaksi e on d.no_transaksi=e.no_transaksi where e.id_customer='$id_user'";
                 $query = $this->db->query($sql);
                 if ($query->num_rows() > 0) {
                     foreach ($query->result() as $row) {
