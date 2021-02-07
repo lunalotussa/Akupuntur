93e96c8c81bab166381eb528b94d18b792dc7f2c
Panji Iman Baskoro <panjidia995@gmail.com>
2021-02-07 23:50:53 +0700

    cart item not updated

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -326,8 +326,10 @@ class Cart extends CI_Controller
                         $purum = array(
                             'status' => '0',
                         );
+
+                        $cuscus = $this->Cart_model->get_id_customer($_SESSION['id_user']);
                         $this->Cart_model->update_transaksi($id_transaksi, $params);
-                        $this->Cart_model->update_cart_user($_SESSION['id_user'], $purum);
+                        $this->Cart_model->update_cart_user($cuscus[0]->id_customer, $purum);
                         redirect('landing/jasa');
                     } else {
                         die("gagal update");
