f7d4a43da7abd0545cd680b4a69d13acab5d6c7c
Panji Iman Baskoro <panjidia995@gmail.com>
2021-02-11 15:53:35 +0700

    test modal:fuck bootstrap

--- application/views/templates/relish/footer.php
+++ application/views/templates/relish/footer.php
@@ -61,22 +61,24 @@
 <!-- SCRIPTS -->
 
 <script src="<?= base_url('assets/relish/js/jquery.min.js'); ?>"></script>
-<script src="<?= base_url('assets/relish/js/bootstrap.min.js'); ?>"></script>
 <script src="<?= base_url('assets/relish/js/jquery.easing.js'); ?>"></script>
 <script src="<?= base_url('assets/relish/js/jquery.sticky.js'); ?>"></script>
 <script src="<?= base_url('assets/relish/js/jquery.form.min.js'); ?>"></script>
 <script src="<?= base_url('assets/relish/js/jquery.validate.min.js'); ?>"></script>
 <script src="<?= base_url('assets/relish/js/jquery.parallax.min.js'); ?>"></script>
 <script src="<?= base_url('assets/relish/js/jquery.appear.js'); ?>"></script>
-<script src="<?= base_url('assets/relish/js/owl.carousel.min.js'); ?>"></script>
-<script src="<?= base_url('assets/relish/js/isotope.pkgd.min.js'); ?>"></script>
 <script src="<?= base_url('assets/relish/js/jquery.fitvids.js'); ?>"></script>
 <script src="<?= base_url('assets/relish/js/jquery.magnific-popup.js'); ?>"></script>
+<script type="text/javascript" src="<?= base_url('assets/relish/revslider/js/jquery.themepunch.tools.min.js'); ?>"></script>
+<script type="text/javascript" src="<?= base_url('assets/relish/revslider/js/jquery.themepunch.revolution.min.js'); ?>"></script>
+<script type="text/javascript" src="<?= base_url('assets/relish/js/jquery.ui.min.js'); ?>"></script>
+<script type="text/javascript" src="<?= base_url('assets/relish/js/jquery.tweet.js'); ?>"></script>
+<script src="<?= base_url('assets/relish/js/bootstrap.min.js'); ?>"></script>
+<script src="<?= base_url('assets/relish/js/owl.carousel.min.js'); ?>"></script>
+<script src="<?= base_url('assets/relish/js/isotope.pkgd.min.js'); ?>"></script>
 <script src="<?= base_url('assets/relish/js/imagesloaded.pkgd.min.js'); ?>"></script>
 <script src="<?= base_url('assets/relish/js/masonry.pkgd.min.js'); ?>"></script>
 <script src="<?= base_url('assets/relish/js/retina.min.js'); ?>"></script>
-<script type="text/javascript" src="<?= base_url('assets/relish/revslider/js/jquery.themepunch.tools.min.js'); ?>"></script>
-<script type="text/javascript" src="<?= base_url('assets/relish/revslider/js/jquery.themepunch.revolution.min.js'); ?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/relish/revslider/js/extensions/revolution.extension.slideanims.min.js'); ?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/relish/revslider/js/extensions/revolution.extension.layeranimation.min.js'); ?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/relish/revslider/js/extensions/revolution.extension.navigation.min.js'); ?>"></script>
@@ -87,8 +89,6 @@
 <script type="text/javascript" src="<?= base_url('assets/relish/revslider/js/extensions/revolution.extension.migration.min.js'); ?>"></script>
 <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script>
 <script type="text/javascript" src="<?= base_url('assets/relish/js/gmap3.min.js'); ?>"></script>
-<script type="text/javascript" src="<?= base_url('assets/relish/js/jquery.ui.min.js'); ?>"></script>
-<script type="text/javascript" src="<?= base_url('assets/relish/js/jquery.tweet.js'); ?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/relish/js/scripts.js'); ?>"></script>
 
 <!-- / SCRIPTS -->
