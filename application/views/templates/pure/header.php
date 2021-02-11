e77a01ed965da9a347994c33672c25a16e038cfc
Panji Iman Baskoro <panjidia995@gmail.com>
2021-02-11 15:58:00 +0700

    test modal:fuck bootstrap

--- application/views/templates/pure/header.php
+++ application/views/templates/pure/header.php
@@ -7,6 +7,7 @@
     <title>Akupuntur</title>
 
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
 </head>
 
