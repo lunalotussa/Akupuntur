aa4e87175de1f168602182f3a887b50885b0f57c
Panji Iman Baskoro <panjidia995@gmail.com>
2021-02-11 18:43:37 +0700

    fix landing

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -183,7 +183,7 @@ class Cart extends CI_Controller
                  $customer_id = $this->Cart_model->add_cart($params);
                  redirect('cart/index');
              }else{
-                $_SESSION['statusTerapis'] = "2";
+                $_SESSION['heyhey'] = "2";
                 redirect('landing/jasa');
             }
         }else{
--- application/views/landing/jasa.php
+++ application/views/landing/jasa.php
@@ -88,7 +88,7 @@
 
         <div class="group mt-40">
           <strong class="text-danger">
-            Pastikan Anda memilih terapis yang sama jika memesan lebih dari 1 layanan <?php var_dump($_SESSION['statusTerapis'] )?>
+            Pastikan Anda memilih terapis yang sama jika memesan lebih dari 1 layanan <?php var_dump($_SESSION['heyhey'] )?>
           </strong>
         </div>
 
