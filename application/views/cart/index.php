e7ddd9e7e03b5bce878ed5b449a475e42b4e6b00
Panji Iman Baskoro <panjidia995@gmail.com>
2021-02-11 15:34:58 +0700

    test modal

--- application/views/cart/index.php
+++ application/views/cart/index.php
@@ -110,7 +110,7 @@
     </div>
 </section>
 <script>
-
+$('#exampleModalLong').modal('show');
 var id_terapis = '<?= $tera_who?>';
 
 function handler(e){
@@ -140,21 +140,21 @@ function handler(e){
 </script>
 
 <!-- Modal -->
-<div class="modal" tabindex="-1" role="dialog">
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <p>Modal body text goes here.</p>
+        ...
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Save changes</button>
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
