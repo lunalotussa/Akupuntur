f02360f63165f0595a746410faa60df876bcb5e3
Gellen <hellogellen@gmail.com>
2021-03-17 15:29:40 +0700

    add notif

--- application/views/templates/pure/footer.php
+++ application/views/templates/pure/footer.php
@@ -21,6 +21,14 @@
                 });
             });
         }
+
+        (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+            const $notification = $delete.parentNode;
+
+            $delete.addEventListener('click', () => {
+                $notification.parentNode.removeChild($notification);
+            });
+        });
     });
 </script>
 </body>
